a692518b1b5ec2eb211ec8c24d4f19da53f7621e
0x2E757 <0x2E757@gmail.com>
2023-03-24 11:44:50 +0200

    Formatted ClientApp files

--- Midis/ClientApp/src/AppRoutes.js
+++ Midis/ClientApp/src/AppRoutes.js
@@ -2,14 +2,14 @@ import { Counter } from "./components/Counter";
 import { Home } from "./components/Home";
 
 const AppRoutes = [
-  {
-    index: true,
-    element: <Home />
-  },
-  {
-    path: '/counter',
-    element: <Counter />
-  }
+    {
+        index: true,
+        element: <Home />
+    },
+    {
+        path: '/counter',
+        element: <Counter />
+    }
 ];
 
 export default AppRoutes;
